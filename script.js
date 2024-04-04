0e3e778bba1d4e8345e786d055f14058cc8f254e
unknown <you@example.com>
2024-04-04 21:02:52 +0600

    first commit

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const createCircleButton = document.getElementById("CircleButton");
-//     const circlesContainer = document.getElementById("circlesContainer");
-//     let clickCount = 0;
-
-//     createCircleButton.addEventListener("click", function() {
-//         clickCount++;
-//         const circle = document.createElement("div");
-//         circle.classList.add("circle");
-//         circle.style.backgroundColor = getRandomColor();
-//         circlesContainer.appendChild(circle);
-//     });
-
-//     function getRandomColor() {
-//         const letters = "0123456789ABCDEF";
-//         let color = "#";
-//         for (let i = 0; i < 6; i++) {
-//             color += letters[Math.floor(Math.random() * 16)];
-//         }
-//         return color;
-//     }
-// });
-
-
 document.addEventListener("DOMContentLoaded", function() {
     const createCircleButton = document.getElementById("CircleButton");
     const circlesContainer = document.getElementById("circlesContainer");
